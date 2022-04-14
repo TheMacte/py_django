0ee8e26c5e4464905220bb06c6d468c04bf984c6
Oleg Dmitriev <i@horo.so>
2022-04-15 02:24:30 +0300

    how share link list for all? ForFRY

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+links_menu = [
+    {'href': 'index', 'title': 'Магазин', 'name': 'домой'},
+    {'href': 'products', 'title': 'Продукты', 'name': 'продукты'},
+    {'href': 'contacts', 'title': 'Контакты', 'name': 'контакты'},
+]
+
 
 def index(request):
-    context = {}
+    context = {'links_menu': links_menu}
     return render(request, 'geekshop/index.html', context)
 
 
 def contacts(request):
-    return render(request, 'geekshop/contacts.html')
+    context = {'links_menu': links_menu}
+    return render(request, 'geekshop/contacts.html', context)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+links_menu = [
+    {'href': 'index', 'title': 'Магазин', 'name': 'домой'},
+    {'href': 'products', 'title': 'Продукты', 'name': 'продукты'},
+    {'href': 'contacts', 'title': 'Контакты', 'name': 'контакты'},
+]
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {'links_menu': links_menu}
+    return render(request, 'mainapp/products.html', context)
